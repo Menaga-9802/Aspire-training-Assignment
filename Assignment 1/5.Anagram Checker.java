bc6edf818bbfbcba1fb7cd51fd453c6ef9d1b69b
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2023-09-27 10:37:17 +0530

    Create 5.Anagram Checker.java

---
+++ Assignment 1/5.Anagram Checker.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main{
+   public static void main(String[] args) {
+      String str1="listen";
+      String str2="silent";
+      if(str1.length()==str2.length()){
+         char ch1[] = str1.toCharArray();
+         char ch2[] = str2.toCharArray();
+         Arrays.sort(ch1);
+         Arrays.sort(ch2);
+         boolean res = Arrays.equals(ch1,ch2);
+         if(res){
+            System.out.println("true");
+         }
+         else{
+            System.out.println("false");
+         }
+      }
+   }
+}
