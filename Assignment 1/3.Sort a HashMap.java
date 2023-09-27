27426297d6452e4b1095265bc2685626aedf508c
MENAGA E <107313942+Menaga-9802@users.noreply.github.com>
2023-09-27 09:23:23 +0530

    Update 3.Sort a HashMap.java

--- Assignment 1/3.Sort a HashMap.java
+++ Assignment 1/3.Sort a HashMap.java
@@ -1,15 +1,12 @@
 import java.util.*;
-class Main{
+public class Main{
     public static void main(String[] args) {
-        HashMap<Integer, String> hashMap = new HashMap<>();
+        Map<Integer,String> hashMap = new HashMap<>();
         hashMap.put(3,"\"apple\"");
         hashMap.put(2,"\"banana\"");
         hashMap.put(1,"\"cherry\"");
-        TreeMap<Integer, String> sortedMap = new TreeMap<>(hashMap);
-        System.out.print("{");
-        for(Map.Entry<Integer, String> entry : sortedMap.entrySet()) {
-            System.out.print(entry.getKey() + " = " + entry.getValue()+",");
-        }
-        System.out.print("}");
+        Map<Integer,String> sortedMap = new TreeMap<>();
+        sortedMap.putAll(hashMap);
+        System.out.println(sortedMap);
     }
 }
